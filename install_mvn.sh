e4d917984ba9c181dc7a7d40786841e496dc3fdb
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-06-18 19:23:53 +1000

    @esteinig - add Dockerfile

--- install_mvn.sh
+++ install_mvn.sh
@@ -1,7 +1,7 @@
 
 
-mvn -U install:install-file -Dfile=./libs/colloquial.jar  -DgroupId=com.colloquial -DartifactId=arithcode         -Dversion=1.1         -Dpackaging=jar
-mvn -U install:install-file -Dfile=./libs/mdsj.jar -DgroupId=de.unikonstanz.inf -DartifactId=mdsj -Dversion=0.2 -Dpackaging=jar 
-mvn -U install:install-file -Dfile=./libs/pal1.5.1.1.jar -DgroupId=nz.ac.auckland -DartifactId=pal -Dversion=1.5.1.1 -Dpackaging=jar 
+mvn install:install-file -Dfile=./libs/colloquial.jar  -DartifactId=arithcode         -Dversion=1.1         -Dpackaging=jar
+mvn install:install-file -Dfile=./libs/mdsj.jar -DartifactId=mdsj -Dversion=0.2 -Dpackaging=jar 
+mvn install:install-file -Dfile=./libs/pal1.5.1.1.jar -DartifactId=pal -Dversion=1.5.1.1 -Dpackaging=jar 
 
 

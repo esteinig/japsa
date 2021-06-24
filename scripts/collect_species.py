c398a2b1980813a6395532cc7fa179eb604f1fe9
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-06-25 08:29:00 +1000

    nextflow

---
+++ scripts/collect_species.py
@@ -0,0 +1,40 @@
+#!/usr/env python
+
+""" Collect the results of the current nested output structure of JAPSA Coverage (SpeciesTyping)
+
+Usage: python scripts/collect_species.py <sample_id> <result_dir> <outfile>
+
+"""
+
+import sys
+
+from pathlib import Path
+
+sample = sys.argv[1]
+results = sys.argv[2]
+output = sys.argv[3]
+
+result_path = Path(results)
+output_file = Path(output)
+
+header = None
+for f in result_path.glob(f"**/{sample}.dat"):
+    if f.parent.name.startswith(sample):
+        db_name = f.parent.parent.name
+    else:
+        db_name = f.parent.name
+
+    db = db_name.lstrip("unmapped.fq.gz").rstrip(".jST")
+
+    with f.open("r") as data_file, f.open("a") as out_file:
+        for line in data_file:
+            if header is None:
+                # First line is header
+                out_file.write(line.strip() + f"\tDatabase\n")
+                header = line
+            else:
+                out_file.write(line.strip() + f"\t{db}\n")
+        
+
+
+
